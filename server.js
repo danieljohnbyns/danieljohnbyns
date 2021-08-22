f2b1e4d2052ead754cebea6aac4e05cd9d4d4522
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2021-08-22 09:45:45 +0800

    heroku is now working

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 const app = express();
 
 // set port
 const port = process.env.PORT || 5500;
 
+// uses
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(path.join(__dirname + '/public/images/favicon.ico')));
 
 // routes
-
 app.get('/', (req, res) => {
     res.render('index');
 });
