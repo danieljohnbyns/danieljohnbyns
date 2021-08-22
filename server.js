28a66832290cd1f9e421a98a838f7697bf3fd8a9
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2021-08-22 08:48:53 +0800

    trying to make heroku work 7

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('this is the server ya\'ll');
+const express = require('express');
+const app = express();
+
+// set port
+const port = process.env.PORT || 5500;
+
+app.use(express.static(__dirname + '/public'));
+
+// routes
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log('app running');
+});
+
+app.set('view engine', 'ejs');
+
+
+
